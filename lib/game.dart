ad4d83d3008b937185150369a8c09c8a71b8b1d6
Tidarat Jiewhwan <jiewhwan_t@silpakorn.edu>
2022-08-10 22:36:36 +0700

    test

--- lib/game.dart
+++ lib/game.dart
@@ -1,20 +1,29 @@
+import 'dart:io';
 import 'dart:math';
 class Game {
   int answer = 0; //instant variable
-  Game(){
+  int count = 0;
+
+  Game() {
     var r = Random();
-    answer = r.nextInt(100) + 1 ;
+    answer = r.nextInt(100) + 1;
+    //print('คำตอบคือ $answer');
   }
-  int doGuess(int num){
-    if(num > answer){
-      print("$num is Too Hight");
+
+  int doGuess(int num) {
+    if (num > answer) {
+      print("➜ $num is Too Hight ▲");
+      count = count+1;
       return 0;
-    }else if (num < answer){
-      print("$num is Too Low");
+    } else if (num < answer) {
+      print("➜ $num is Too Low ▼");
+      count = count+1;
       return 0;
-    }else{
-      print("$num is Too Correct");
+    } else {
+      count = count+1;
+      print("➜ $num is Too Correct ❤, total guess is $count");
       return 1;
     }
   }
 }
+
--- lib/hello.dart
+++ lib/hello.dart
@@ -4,18 +4,19 @@ import 'game.dart';
 void main(){
   var game = Game();
   int result = 0;
-  do {
-    stdout.write('Please guess the number between 1 and 100 : ');
-    var input = stdin.readLineSync(); //รับข้อมูลที่เราพิมพ์
-    var guess = int.tryParse(input!); //แปลง String ไปเป็น int
 
-    if(guess == null){
-      print("Please ennter number only");
-      continue;
+
+    do {
+      stdout.write('Please guess the number between 1 and 100 : ');
+      var input = stdin.readLineSync(); //รับข้อมูลที่เราพิมพ์
+      var guess = int.tryParse(input!); //แปลง String ไปเป็น int
+
+      if (guess == null) {
+        continue;
+      }
+      result = game.doGuess(guess);
     }
-    result = game.doGuess(guess);
-
-  }while(result != 1);
+    while (result != 1);
 }
 
 
